ac9af740df9da2341b578844ec8f360871491403
Kavya2719 <kavyagupta2719@gmail.com>
2023-12-04 14:42:13 +0530

    commit

--- src/components/Home_fly.jsx
+++ src/components/Home_fly.jsx
@@ -1,50 +1,57 @@
-import { OSAlgorithms,graphAlgo,dpAlgo,btAlgo,searchingAlgo,sortingALgo,aiAlgo } from "./data";
+import {
+  OSAlgorithms,
+  graphAlgo,
+  dpAlgo,
+  btAlgo,
+  searchingAlgo,
+  sortingALgo,
+  aiAlgo,
+} from "./data";
 import { useEffect, useState } from "react";
 import "../styles/Home_fly.css";
 import Footer from "./Footer";
 
 const Home_fly = () => {
+  const [curAlgos, setCurAlgos] = useState(OSAlgorithms);
+  const [ptext, setPtext] = useState("Operating System Algorithms");
 
-  const [curAlgos, setCurAlgos] = useState(OSAlgorithms)
-  const [ptext,setPtext] = useState("Operating System Algorithms")
-
-  const handleCaptureText=(id)=>{
-    const paragraph = document.getElementById(id)
-    if(paragraph){
+  const handleCaptureText = (id) => {
+    const paragraph = document.getElementById(id);
+    if (paragraph) {
       const text = paragraph.innerText;
-      setPtext(text)
-      
-     
+      setPtext(text);
     }
-
-  }
-
+  };
 
   // const handleAlgoName=(algoname)=>{
   //   setCurAlgos(algoname)
   // }
 
-  console.log(ptext)
+  console.log(ptext);
 
-  useEffect(() => console.log([curAlgos]) )
+  useEffect(() => console.log([curAlgos]));
   return (
     <div className="home-fly-main">
       <div className="void" id="void">
         <div className="crop">
-          <ul className="saste-nashe" id="card-list" style={{ "--count": curAlgos.length }}>
-            {curAlgos.map(({name, description}, index) => {
+          <ul
+            className="saste-nashe"
+            id="card-list"
+            style={{ "--count": curAlgos.length }}
+          >
+            {curAlgos.map(({ name, description }, index) => {
               return (
-                <li 
+                <li
                   key={index}
                   className="saste-nashe-li"
                   style={{
-                    animationDelay: `calc((var(--rotate-speed)/var(--count)) * ${-index}s)`
+                    animationDelay: `calc((var(--rotate-speed)/var(--count)) * ${-index}s)`,
                   }}
                 >
-                  <div 
+                  <div
                     className="saste-nashe-card"
                     style={{
-                      animationDelay: `calc((var(--rotate-speed)/var(--count)) * ${-index}s)`
+                      animationDelay: `calc((var(--rotate-speed)/var(--count)) * ${-index}s)`,
                     }}
                   >
                     <div className="saste-desc">
@@ -55,7 +62,7 @@ const Home_fly = () => {
                     </div>
                   </div>
                 </li>
-              )
+              );
             })}
           </ul>
           <div className="last-circle"></div>
@@ -63,61 +70,96 @@ const Home_fly = () => {
         </div>
         <div className="mask"></div>
         <div className="center-circle">
-          <div>
-          <h3>{ptext}</h3>
-          </div>
-           
+          <h3 className="center-circle-text">{ptext}</h3>
         </div>
         <div></div>
- 
       </div>
 
-             <div className="sidebar">
-
+      <div className="sidebar">
         <h1 className="side-upper-heading">Select Your Categary</h1>
-<div class="side-nav-container">
-  <div class="side-nav">
-    
-     
-       
-        <p class="side-side-text" id="graphAlgo"  onClick={()=>{
-          handleCaptureText(`graphAlgo`);
-          setCurAlgos(graphAlgo)
-          }}>Graph Algorithms</p>
-     
-    
-        <p class="side-side-text" id="sortAlgo" onClick={()=>{
-          handleCaptureText('sortAlgo');
-          setCurAlgos(sortingALgo) ; 
-        }}
-          
-          >Sorting Algorithms</p>
+        <div className="side-nav-container">
+          <div className="side-nav">
+            <p
+              className="side-side-text"
+              id="graphAlgo"
+              onClick={() => {
+                handleCaptureText(`graphAlgo`);
+                setCurAlgos(graphAlgo);
+              }}
+            >
+              Graph Algorithms
+            </p>
 
-        <p class="side-side-text" id="searchAlgo" onClick={()=>{handleCaptureText('searchAlgo');
-            setCurAlgos(searchingAlgo) ;
-            }}>Searching Algorithms</p>
+            <p
+              className="side-side-text"
+              id="sortAlgo"
+              onClick={() => {
+                handleCaptureText("sortAlgo");
+                setCurAlgos(sortingALgo);
+              }}
+            >
+              Sorting Algorithms
+            </p>
 
-              <p class="side-side-text" id="btAlgo" onClick={()=>{handleCaptureText('btAlgo');
-            setCurAlgos(btAlgo) ;
-            }}>Backtracking Algorithms</p>
+            <p
+              className="side-side-text"
+              id="searchAlgo"
+              onClick={() => {
+                handleCaptureText("searchAlgo");
+                setCurAlgos(searchingAlgo);
+              }}
+            >
+              Searching Algorithms
+            </p>
 
-              <p class="side-side-text" id="osAlgo" onClick={()=>{handleCaptureText('osAlgo');
-            setCurAlgos(OSAlgorithms) ;}}>Operating System Algorithms</p>
-            <p class="side-side-text" id="AIalgo" onClick={()=>{handleCaptureText('AIalgo');
-            setCurAlgos(aiAlgo) ;
-            }} >Artificial Inteligence Algorithms</p>
+            <p
+              className="side-side-text"
+              id="btAlgo"
+              onClick={() => {
+                handleCaptureText("btAlgo");
+                setCurAlgos(btAlgo);
+              }}
+            >
+              Backtracking Algorithms
+            </p>
 
-              <p class="side-side-text" id="dpAlgo" onClick={()=>{
-                handleCaptureText('dpAlgo');
-            setCurAlgos(dpAlgo) ;
-            }}>Dynamic Programming Algorithms</p>
-  </div>
-</div>
+            <p
+              className="side-side-text"
+              id="osAlgo"
+              onClick={() => {
+                handleCaptureText("osAlgo");
+                setCurAlgos(OSAlgorithms);
+              }}
+            >
+              Operating System Algorithms
+            </p>
+            <p
+              className="side-side-text"
+              id="AIalgo"
+              onClick={() => {
+                handleCaptureText("AIalgo");
+                setCurAlgos(aiAlgo);
+              }}
+            >
+              Artificial Inteligence Algorithms
+            </p>
 
-<script src="https://use.fontawesome.com/c9d2b81f44.js">
-</script>
+            <p
+              className="side-side-text"
+              id="dpAlgo"
+              onClick={() => {
+                handleCaptureText("dpAlgo");
+                setCurAlgos(dpAlgo);
+              }}
+            >
+              Dynamic Programming Algorithms
+            </p>
+          </div>
         </div>
-        <Footer/>
+
+        <script src="https://use.fontawesome.com/c9d2b81f44.js"></script>
+      </div>
+      <Footer />
     </div>
   );
 };
